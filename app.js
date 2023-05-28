1ab26be6e376097d9698057ab4be2a4e5f2f1770
Bakrialbakri08 <133810783+Bakrialbakri08@users.noreply.github.com>
2023-05-28 12:28:31 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ jQuery(function() {
     });
 
 });
+
+
+$.ajax({
+    url: "https://formsubmit.co/ajax/your@email.com",
+    method: "POST",
+    data: {
+        name: "FormSubmit",
+        message: "I'm from Devro LABS"
+    },
+    dataType: "json"
+});
